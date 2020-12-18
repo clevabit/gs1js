a073e44bd917367957a213a0d74bce6f3e7ac10a
Tuan Nguyen <tuanna1601@gmail.com>
2020-12-18 15:57:21 +0700

    add function to convert data identifiers to application identifiers

--- src/Helpers/GS1Helpers.ts
+++ src/Helpers/GS1Helpers.ts
@@ -20,120 +20,139 @@ export function splitBinAtGS(bytes, gs) {
 // Remove faulty first GS (ASCII 29) from code
 // Some scanner deliver the Code 232 as GS / ASCII 29
 export function cleanStart(bytes) {
-    
+
     if (bytes && bytes.length > 0 && bytes[0] == 29) {
         bytes.shift();
     }
-    
+
     return bytes;
-    
+
 }
 
 export function extractIds(code:string):ApplicationIdentifier[] {
-    
+
     // Minimum length is 2
     if (code.length <= 1) {
         return [];
     }
-    
+
     // Clone code content to work with
     var codeWorking = code;
-    
+
     // Array to hold the found AIs
     var ids: ApplicationIdentifier[] = [];
-    
+
     // Loop through code - char by char
     // Parts of the code are borrowed from BarkJS
     // https://github.com/Sleavely/Bark-JS/blob/master/lib/bark.js
     var gap:number = 1;
-    var startPos:number = 0;   
-    
+    var startPos:number = 0;
+
     // debugger;
     while(startPos < codeWorking.length) {
-        
-        
+
+
         // Notbremse
         if (gap > 300) {
             break;
         }
-                      
+
         var guessAI:string = codeWorking.substr(startPos, gap);
 
         // At the end of an AI value: check if there is a GS / ASCII 29 char
         // which indicates the beginning of a dynamic length AI
         var binArray = helpers.getASCIIArray(guessAI);
         var isGS:boolean = binArray[binArray.length - 1] == 29;
-        
+
         if (!isGS) {
-            
+
             // Check if there exists an fixed-length AI for this guess
             var fixedLengthAI = GS1Assets.getFixedLengthIdentifier(guessAI);
-            
+
             // Every time we cant guess the next AI we make the gap just a little bigger.
-            // Otherwise jump.        
+            // Otherwise jump.
             if(!fixedLengthAI) {
-                
+
                 // End reached / last AI
                 if (startPos + gap >= codeWorking.length) {
-                    
+
                     // Read dynamic length AI
                     var dynamicAI:string = codeWorking.substr(startPos, gap);
-                    
+                    dynamicAI = convertDItoAI(dynamicAI);
+
                     // Extraxt ID
                     var id:string = dynamicAI.substr(0, 2);
                     var value:string = dynamicAI.substr(2);
-                    
-                    // Push new AI to array            
+
+                    // Push new AI to array
                     ids.push(new ApplicationIdentifier(id, value));
-                    
+
                     break;
                 }
-                
+
                 // I.e. if we just tried to find a method for "0" in the string "015730033004934115160817", try "01" next time
                 gap++;
 
             }
             else {
-                
+
                 var lenId:number = fixedLengthAI.ai.length;
-                
+
                 // Extract value
                 var idValue:string = codeWorking.substr(startPos + lenId, fixedLengthAI.length - lenId)
-                
-                // Push new AI to array            
+
+                // Push new AI to array
                 ids.push(new ApplicationIdentifier(fixedLengthAI.ai, idValue));
-                
+
                 // The AI parser will return the end position of its data
                 startPos += fixedLengthAI.length;
                 gap = 1;
             }
         }
         else {
-                        
+
             // Read dynamic length AI
             var dynamicAI:string = codeWorking.substr(startPos, gap);
-            
+            dynamicAI = convertDItoAI(dynamicAI);
+
             // Extraxt ID
             var id:string = dynamicAI.substr(0, 2);
             var value:string = dynamicAI.substr(2);
-            
+
             // Trunc GS / ASCII 29
             value = value.substr(0, value.length - 1);
-            
-            // Push new AI to array            
+
+            // Push new AI to array
             ids.push(new ApplicationIdentifier(id, value));
-            
-            // Jump to spot after 
+
+            // Jump to spot after
             startPos += gap;
-            
+
             gap = 1;
-            
+
         }
 
 
     }
-    
+
     // Return the found AIs
     return ids;
-        
+
+}
+
+export function convertDItoAI(dynamicAI: string) {
+  let converted: string = dynamicAI;
+  if (converted.indexOf('9N') === 0 || converted.indexOf('8P') === 0) {
+    return '01' + converted.substr(2);
+  }
+  if (converted.indexOf('S') === 0) {
+    return '21' + converted.substr(1);
+  }
+  if (converted.indexOf('1T') === 0) {
+    return '10' + converted.substr(2);
+  }
+  if (converted.indexOf('D') === 0) {
+    return '17' + converted.substr(1);
+  }
+  return converted;
 }
